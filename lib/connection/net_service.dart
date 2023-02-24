e8ea3304dd571b0a2b97cf882c2d65adbc8a4a66
Jonathan <jdrx94@outlook.com>
2023-02-24 13:50:23 +0100

    OK

--- lib/connection/net_service.dart
+++ lib/connection/net_service.dart
@@ -1,16 +1,15 @@
 import 'dart:convert';
-import 'package:plazaleku/models/Parking.dart';
 
 import 'package:http/http.dart' as http;
 
-Future<Parking> fetchAlbum() async {
+Future<List<dynamic>> fetchParking() async {
   final response = await http.get(Uri.parse(
       'https://www.donostia.eus/info/ciudadano/camaras_trafico.nsf/getParkings.xsp'));
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
-    return Parking.fromJson(jsonDecode(response.body));
+    return jsonDecode(response.body)['features'];
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
--- lib/screens/tabs/Lista.dart
+++ lib/screens/tabs/Lista.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plazaleku/connection/net_service.dart';
 
 class Lista extends StatefulWidget {
   const Lista({super.key});
@@ -8,8 +9,40 @@ class Lista extends StatefulWidget {
 }
 
 class _ListaState extends State<Lista> {
+  late Future<List<dynamic>> futureParking;
+
+  @override
+  void initState() {
+    super.initState();
+    futureParking = fetchParking();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: FutureBuilder(
+        future: futureParking,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                var item = snapshot.data![index];
+                return Card(
+                  child: ListTile(
+                    title: Text(item['properties']['nombre'].toString()),
+                  ),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+
+          // By default, show a loading spinner.
+          return const CircularProgressIndicator();
+        },
+      ),
+    );
   }
 }
