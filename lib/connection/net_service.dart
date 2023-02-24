1e8ae4930ca830d2006cf8576e8e3edb3fbadc09
Jonathan <jdrx94@outlook.com>
2023-02-24 12:24:50 +0100

    Update

--- lib/connection/net_service.dart
+++ lib/connection/net_service.dart
@@ -1,17 +1,5 @@
-import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
-class Conexion {
-  static Future<List<String>> obtenerXML() async {
-    Uri url = Uri.parse(
-        "https://www.donostia.eus/info/ciudadano/camaras_trafico.nsf/getParkings.xsp");
-    final response = await http.get(url);
-    if (response.statusCode == 200) {
-      var data = jsonDecode(response.body);
-      return data;
-    } else {
-      throw Exception('Error al cargar el XML');
-    }
-  }
+Future<http.Response> fetchAlbum() {
+  return http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
 }
