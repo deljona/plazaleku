c03c51d74112063c0bc32d6aefe8770251373398
Jonathan <jdrx94@outlook.com>
2023-02-24 12:38:07 +0100

    Update

--- lib/connection/net_service.dart
+++ lib/connection/net_service.dart
@@ -1,5 +1,19 @@
+import 'dart:convert';
+import 'package:plazaleku/models/Parking.dart';
+
 import 'package:http/http.dart' as http;
 
-Future<http.Response> fetchAlbum() {
-  return http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+Future<Parking> fetchAlbum() async {
+  final response = await http.get(Uri.parse(
+      'https://www.donostia.eus/info/ciudadano/camaras_trafico.nsf/getParkings.xsp'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Parking.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('No se pudieron cargar los datos.');
+  }
 }
