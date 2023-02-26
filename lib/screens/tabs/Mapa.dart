904a5b4846b3a4a23ca3b87a7af52ca2a933883a
Jonathan <jdrx94@outlook.com>
2023-02-26 21:41:20 +0100

    OK

--- lib/screens/tabs/Mapa.dart
+++ lib/screens/tabs/Mapa.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Mapa extends StatefulWidget {
   const Mapa({super.key});
@@ -8,14 +9,23 @@ class Mapa extends StatefulWidget {
 }
 
 class _MapaState extends State<Mapa> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: const Icon(
-        Icons.map_rounded,
-        size: 40,
+    return Scaffold(
+        body: GoogleMap(
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: CameraPosition(
+        target: _center,
+        zoom: 11.0,
       ),
-    );
+    ));
   }
 }
